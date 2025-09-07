55eaa04719d890bad91b69d521379ffeb3288a6a
Jeremy <xinghong.su0818@gmail.com>
2025-09-07 19:44:15 +0800

    更新程式碼註解、electron-run.js端口映射錯誤

--- electron-run.js
+++ electron-run.js
@@ -7,18 +7,18 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1000,
         height: 700,
-        autoHideMenuBar: true, // ✅ 自動隱藏 menu bar
+        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
         },
     });
 
-    // ✅ 移除選單
+    // 移除選單
     Menu.setApplicationMenu(null);
 
-    // 映射到你本地 9000 port 的網頁
-    mainWindow.loadURL('http://localhost:9000');
+    // 載入本地伺服器的 URL
+    mainWindow.loadURL('http://localhost:9393');
 
     mainWindow.on('closed', () => {
         mainWindow = null;
